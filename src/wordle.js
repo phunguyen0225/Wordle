d8919df307d48d17700ba7dfb30baab159703f02
Phu Nguyen <phunguyen0225@gmail.com>
2022-02-19 17:23:33 -0600

    refactoring with functions

--- src/wordle.js
+++ src/wordle.js
@@ -8,37 +8,51 @@ const Match = {
 
 const WORD_SIZE = 5;
 
-function count(str) {
-  return str.split('').reduce((acc, curr) => {
-    if (acc[curr]) {
-      acc[curr]++;
-    } else {
-      acc[curr] = 1;
-    }
-    return acc;
-  }, {});
+function count_postion(guess, target, letter) {
+  let count = 0;
+  for (let i = 0; i < WORD_SIZE; i++) {
+    if (guess[i] === letter && target[i] === letter)
+      count++;
+  }
+
+  return count;
+}
+
+
+function count_until_position(position, word, letter) {
+  let count = 0;
+  for (let i = 0; i <= position; i++) {
+    if (word[i] === letter)
+      count++;
+  }
+
+  return count;
+}
+
+
+function tallyForPosition(position, guess, target) {
+  const theLetter = guess[position];
+  const numOccur = count_until_position(position, guess, theLetter);
+  const non_exact = count_until_position(WORD_SIZE, target, theLetter) - count_postion(guess, target, theLetter);
+
+  if (target[position] === guess[position]) {
+    return 'Exact';
+  } else if (non_exact >= numOccur) {
+    return 'Match';
+  } else {
+    return 'NO_MATCH';
+  }
 }
 
 function tally(guess, target) {
   const response = new Array(WORD_SIZE).fill('NO_MATCH');
-  const target_count = count(target);
 
   if (guess.length !== WORD_SIZE) {
     throw new Error('Invalid guess');
   }
 
   for (let i = 0; i < WORD_SIZE; i++) {
-    if (guess[i] === target[i]) {
-      response[i] = Match.EXACT;
-      target_count[guess[i]]--;
-    }
-  }
-
-  for (let i = 0; i < WORD_SIZE; i++) {
-    if (response[i] === Match.NO_MATCH && target_count[guess[i]]) {
-      response[i] = Match.MATCH;
-      target_count[guess[i]]--;
-    }
+    response[i] = tallyForPosition(i, guess, target);
   }
 
   return response;
