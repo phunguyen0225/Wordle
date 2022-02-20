85480df76dd239cc01760969612c258ef1578748
Phu Nguyen <phunguyen0225@gmail.com>
2022-02-19 22:52:12 -0600

    more refactoring with reduce and filter and ternary condition

--- src/wordle.js
+++ src/wordle.js
@@ -8,40 +8,31 @@ const Match = {
 
 const WORD_SIZE = 5;
 
-function count_postion(guess, target, letter) {
-  let count = 0;
-  for (let i = 0; i < WORD_SIZE; i++) {
-    if (guess[i] === letter && target[i] === letter)
-      count++;
-  }
-
-  return count;
+function count_positional_matches(guess, target, letter) {
+  return guess.split('')
+    .filter((char, index) => target[index] === guess[index])
+    .filter(char => char === letter)
+    .length;
 }
 
 
 function count_until_position(position, word, letter) {
-  let count = 0;
-  for (let i = 0; i <= position; i++) {
-    if (word[i] === letter)
-      count++;
-  }
-
-  return count;
+  return word.split('').slice(0, position + 1)
+    .filter((char) => char === letter)
+    .length;
 }
 
 
 function tallyForPosition(position, guess, target) {
   const theLetter = guess[position];
   const numOccur = count_until_position(position, guess, theLetter);
-  const non_exact = count_until_position(WORD_SIZE, target, theLetter) - count_postion(guess, target, theLetter);
+  const non_exact = count_until_position(WORD_SIZE, target, theLetter) - count_positional_matches(guess, target, theLetter);
 
   if (target[position] === guess[position]) {
-    return 'Exact';
-  } else if (non_exact >= numOccur) {
-    return 'Match';
-  } else {
-    return 'NO_MATCH';
+    return Match.EXACT;
   }
+
+  return non_exact >= numOccur ? Match.MATCH : Match.NO_MATCH;
 }
 
 function tally(guess, target) {
@@ -51,11 +42,8 @@ function tally(guess, target) {
     throw new Error('Invalid guess');
   }
 
-  for (let i = 0; i < WORD_SIZE; i++) {
-    response[i] = tallyForPosition(i, guess, target);
-  }
-
-  return response;
+  return guess.split('')
+    .reduce((response, letter, i) => [...response, tallyForPosition(i, guess, target)], []);
 }
 
 module.exports = { tally, Match };
