ef47f62a6ada8d522534074aa8eb83baa1361342
Phu Nguyen <phunguyen0225@gmail.com>
2022-02-22 22:48:16 -0600

    button fixed

--- GUI/grid.js
+++ GUI/grid.js
@@ -1,61 +1,59 @@
-var column = 5;
-var row = 6;
+'use strict';
 
-var currentColumn = 0;
-var currentRow = 0;
+const column = 5;
+const row = 6;
 
-var gameOver = false;
-var word = 'FAVOR';
+let currentColumn = 0;
+const currentRow = 0;
+
+let gameOver = false;
+const word = 'FAVOR';
 
 window.onload = function () {
     initialize();
-}
+};
 
 function initialize() {
     for (let i = 0; i < row; i++) {
         for (let j = 0; j < column; j++) {
-            let cell = document.createElement("span");
-            cell.id = i.toString() + "-" + j.toString(); //0-1
-            cell.classList.add("cell");
-            cell.innerText = "";
-            document.getElementById("grid").appendChild(cell);
+            const cell = document.createElement('span');
+            cell.id = i.toString() + '-' + j.toString(); //0-1
+            cell.classList.add('cell');
+            cell.innerText = '';
+            document.getElementById('grid').appendChild(cell);
         }
     }
 
-    document.addEventListener("keyup", (event) => {
+    document.addEventListener('keyup', (event) => {
         if (gameOver) return;
 
-        if ("KeyA" <= event.code && event.code <= "KeyZ") {
+        if ('KeyA' <= event.code && event.code <= 'KeyZ') {
             if (currentColumn < column) {
-                let currentCell = document.getElementById(currentRow.toString() + "-" + currentColumn.toString());
-                if (currentCell.innerText == "") {
+                const currentCell = document.getElementById(currentRow.toString() + '-' + currentColumn.toString());
+                if (currentCell.innerText === '') {
                     currentCell.innerText = event.code[3];
                     currentColumn += 1;
                 }
             }
-        } else if (event.code == "Backspace") {
+        } else if (event.code === 'Backspace') {
+            document.getElementById('guess').disabled = true;
             if (0 < currentColumn && currentColumn <= column) {
                 currentColumn -= 1;
             }
-            let currentCell = document.getElementById(currentRow.toString() + "-" + currentColumn.toString());
-            currentCell.innerText = "";
+            const currentCell = document.getElementById(currentRow.toString() + '-' + currentColumn.toString());
+            currentCell.innerText = '';
         }
 
-        let filledCell = document.getElementById(currentRow.toString() + "-" + (column - 1).toString());
-        if (filledCell.innerText !== "") {
-            document.getElementById("guess").removeAttribute("disabled");
+        const filledCell = document.getElementById(currentRow.toString() + '-' + (column - 1).toString());
+
+        if (filledCell.innerText !== '') {
+            document.getElementById('guess').disabled = false;
         }
 
-        // else if (document.getElementById("guess").click = true) {
-        //     reset();
-        //     currentRow += 1;
-        //     currentColumn = 0;
-        // }
-
-        if (!gameOver && currentRow == row) {
+        if (!gameOver && currentRow === row) {
             gameOver = true;
         }
-    })
+    });
 }
 
 function reset() {
