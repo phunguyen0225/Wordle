c28fac9e1146dfd8bb97abc49bb6e39cf849b4f1
Phu Nguyen <phunguyen0225@gmail.com>
2022-02-22 21:23:14 -0600

    working on UI

--- GUI/grid.js
+++ GUI/grid.js
@@ -1,8 +1,8 @@
-const column = 5;
-const row = 6;
+var column = 5;
+var row = 6;
 
-const currentColumn = 0;
-const currentRow = 0;
+var currentColumn = 0;
+var currentRow = 0;
 
 var gameOver = false;
 var word = 'FAVOR';
@@ -16,10 +16,49 @@ function initialize() {
         for (let j = 0; j < column; j++) {
             let cell = document.createElement("span");
             cell.id = i.toString() + "-" + j.toString(); //0-1
-            cell.classList("cell");
-            cell.innerText = "P";
+            cell.classList.add("cell");
+            cell.innerText = "";
             document.getElementById("grid").appendChild(cell);
         }
     }
+
+    document.addEventListener("keyup", (event) => {
+        if (gameOver) return;
+
+        if ("KeyA" <= event.code && event.code <= "KeyZ") {
+            if (currentColumn < column) {
+                let currentCell = document.getElementById(currentRow.toString() + "-" + currentColumn.toString());
+                if (currentCell.innerText == "") {
+                    currentCell.innerText = event.code[3];
+                    currentColumn += 1;
+                }
+            }
+        } else if (event.code == "Backspace") {
+            if (0 < currentColumn && currentColumn <= column) {
+                currentColumn -= 1;
+            }
+            let currentCell = document.getElementById(currentRow.toString() + "-" + currentColumn.toString());
+            currentCell.innerText = "";
+        }
+
+        let filledCell = document.getElementById(currentRow.toString() + "-" + (column - 1).toString());
+        if (filledCell.innerText !== "") {
+            document.getElementById("guess").removeAttribute("disabled");
+        }
+
+        // else if (document.getElementById("guess").click = true) {
+        //     reset();
+        //     currentRow += 1;
+        //     currentColumn = 0;
+        // }
+
+        if (!gameOver && currentRow == row) {
+            gameOver = true;
+        }
+    })
+}
+
+function reset() {
+
 }
 
