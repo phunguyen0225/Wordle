7c403244371095cde15fd79af91dd02adc05315c
Phu Nguyen <phunguyen0225@gmail.com>
2022-02-23 20:48:03 -0600

    fixing the currentrow and column

--- GUI/grid.js
+++ GUI/grid.js
@@ -4,7 +4,7 @@ const column = 5;
 const row = 6;
 
 let currentColumn = 0;
-const currentRow = 0;
+let currentRow = 0;
 
 let gameOver = false;
 const target = 'FAVOR';
@@ -14,42 +14,46 @@ window.onload = function () {
 };
 
 function initialize() {
+    //setting the grid
     for (let i = 0; i < row; i++) {
         for (let j = 0; j < column; j++) {
             const cell = document.createElement('span');
-            cell.id = i.toString() + '-' + j.toString(); //0-1
+            cell.id = i.toString() + '-' + j.toString();   //set the id to "0-1"
             cell.classList.add('cell');
             cell.innerText = '';
             document.getElementById('grid').appendChild(cell);
         }
     }
-
+    //listen for key
     document.addEventListener('keyup', (event) => {
         if (gameOver) return;
 
-        if ('KeyA' <= event.code && event.code <= 'KeyZ') {
-            if (currentColumn < column) {
-                const currentCell = document.getElementById(currentRow.toString() + '-' + currentColumn.toString());
+        if ('KeyA' <= event.code && event.code <= 'KeyZ') { //if between A-Z
+            if (currentColumn < column) {       //if not out of bound
+                const currentCell = document.getElementById(currentRow.toString() + '-' + currentColumn.toString()); //get the id if currentRow and column
                 if (currentCell.innerText === '') {
-                    currentCell.innerText = event.code[3];
+                    currentCell.innerText = event.code[3];   //KeyA so A is index [3]
                     currentColumn += 1;
                 }
             }
-        } else if (event.code === 'Backspace') {
+        } else if (event.code === 'Backspace') {   //delete inbound and set equal to empty string
             document.getElementById('guess').disabled = true;
             if (0 < currentColumn && currentColumn <= column) {
                 currentColumn -= 1;
             }
+
             const currentCell = document.getElementById(currentRow.toString() + '-' + currentColumn.toString());
             currentCell.innerText = '';
         }
 
-        const filledCell = document.getElementById(currentRow.toString() + '-' + (column - 1).toString());
+        const filledCell = document.getElementById(currentRow.toString() + '-' + (column - 1).toString()); //geting the id of currentrow and last column
 
-        if (filledCell.innerText !== '') {
+        if (filledCell.innerText !== '') {  //disable guess button if the last column is not empty
             document.getElementById('guess').disabled = false;
             const clicked = document.getElementById('guess');
             clicked.addEventListener('click', update);
+            currentRow += 1;     //move to the next row
+            currentColumn = 0;   //reset column at the start
         }
 
         if (!gameOver && currentRow === row) {
@@ -69,23 +73,23 @@ function readGuess() {
 
 function display(numberOfAttempts, status, matchResponse, message) {
     if (status === 'Won') {
-        for (let item of matchResponse) {
-            for (let col = 0; col < column; col++) {
-                const currentCell = document.getElementById(currentRow.toString() + '-' + col.toString());
-                currentCell.classList.add('exact');
-            }
+        for (let col = 0; col < column; col++) {
+            const currentCell = document.getElementById(currentRow.toString() + '-' + col.toString());
+            currentCell.classList.add('exact');
         }
+
         console.log(message);
         gameOver = true;
+
     } else if (status === 'In progress') {
         for (let col = 0; col < column; col++) {
             const currentCell = document.getElementById(currentRow.toString() + '-' + col.toString());
             if (matchResponse[col] === 'Exact') {
-                currentCell.classList.add('exact')
+                currentCell.classList.add('exact');
             } else if (matchResponse[col] === 'Match') {
-                currentCell.classList.add('match')
+                currentCell.classList.add('match');
             } else if (matchResponse[col] === 'NO_MATCH') {
-                currentCell.classList.add('noMatch')
+                currentCell.classList.add('noMatch');
             }
         }
 
